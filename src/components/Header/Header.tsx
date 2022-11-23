1e9f2359a6a2770b248db8bb31741b96d29e8182
Anatolii Kiguradze <anatoliikiguradze@Anatoliis-MBP.Dlink>
2022-11-23 12:57:44 +0600

    created main components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import './header.css';
+
+const Header = () => {
+    return (
+        <div id="header">
+
+            <button id="add-new-words">Add New Words</button>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/MainContainer/MainContainer.tsx
@@ -0,0 +1,9 @@
+import './main-container.css';
+
+const MainContainer = () => {
+    return (
+        <div id="main-container"></div>
+    )
+}
+
+export default MainContainer;
