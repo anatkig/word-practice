c0f27a7cb0966da1ee39fb8cc17e928433a75aca
Anatolii Kiguradze <anatoliikiguradze@Anatoliis-MacBook-Pro.local>
2022-12-06 15:52:31 +0600

    fixes

--- src/logic/localStorageWrite.ts
+++ src/logic/localStorageWrite.ts
@@ -5,6 +5,8 @@ const localStorageWrite = (array: StoredData[], name: string) => {
     if (array.length && name) {
         const stringified = JSON.stringify(array);
         localStorage.setItem(name, stringified);
+    } else if (array && name) {
+        localStorage.setItem(name, "[]");
     }
 
 }
