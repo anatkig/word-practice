8ac6b04eff1feb84fef9eaafa133db7c29ce717d
Anatolii Kiguradze <anatoliikiguradze@Anatoliis-MBP.Dlink>
2022-12-07 12:26:06 +0600

    fixes

--- src/components/WriteBox/WriteBox.tsx
+++ src/components/WriteBox/WriteBox.tsx
@@ -64,7 +64,7 @@ const WriteBox = ({ currentWords, setCurrentWords, setLearntWords, setCount, set
             setInputValue("");
             const currentUnit = currentWords[wordIndex as number];
 
-            if (currentUnit.hit >= (4 + Math.floor(currentUnit.gu.length / 5))) {
+            if (currentUnit.hit >= (4 + Math.round(currentUnit.gu.length / 7))) {
                 setCurrentWords((prev: StoredData[]) => [...prev.filter((unit: StoredData, index: number) => index !== wordIndex)]);
 
                 if (currentUnit.step && currentUnit.session) {
